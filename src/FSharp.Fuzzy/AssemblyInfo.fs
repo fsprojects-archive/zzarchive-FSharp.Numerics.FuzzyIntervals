c5df4c60231efbd439fb96a4fa26394f5e6c45d3
dsevastianov <dsevastianov@gmail.com>
2013-12-20 17:31:04 -0500

    some nuget work

---
+++ src/FSharp.Fuzzy/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("FSharp.Fuzzy")>]
+[<assembly: AssemblyProductAttribute("FSharp.Fuzzy")>]
+[<assembly: AssemblyDescriptionAttribute("Basic libary of fuzzy and interval calculus in F#.")>]
+[<assembly: AssemblyVersionAttribute("0.0.0")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "0.0.0"
